30dfc3aac9db61bd6a247231403640d70803388c
MikhailT <m.tyrysin@gmail.com>
2023-02-13 23:20:11 +0300

    Added Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+string number = (Console.ReadLine());
+if (number == "1")
+
+    Console.WriteLine($"Заданный номер является Понедельником");
+else if (number == "2")
+{
+    Console.WriteLine($"Заданный номер является Вторником");
+}
+else if (number == "3")
+{
+    Console.WriteLine($"Заданный номер является Средой");
+}
+else if (number == "4")
+{
+    Console.WriteLine($"Заданный номер является Четвергом");
+}
+else if (number == "5")
+{
+    Console.WriteLine($"Заданный номер является Пятницей");
+}
+else if (number == "6")
+{
+    Console.WriteLine($"Заданный номер является Субботой");
+}
+else if (number == "7")
+{
+    Console.WriteLine($"Заданный номер является Воскресеньем");
+}
+else
+{
+    Console.WriteLine($"Заданный номер не является днем недели");
+}
