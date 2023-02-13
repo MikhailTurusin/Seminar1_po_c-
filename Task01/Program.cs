bab965631ad6825f3dfe952776ea142b823e0411
MikhailT <m.tyrysin@gmail.com>
2023-02-13 22:54:40 +0300

    Added Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+        
+        if (number == number1 * number1)
+        {
+            Console.WriteLine($"Число {number} является квадратом числа {number1}");
+        }
+        else
+        {
+        Console.WriteLine($"Число {number} не является квадратом числа {number1}");
+        }
