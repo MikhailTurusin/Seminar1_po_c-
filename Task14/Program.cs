e0a9586794f430f6418cbd6a6a1ade3dd9109ca3
MikhailT <m.tyrysin@gmail.com>
2023-02-16 00:11:22 +0300

    Added Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно
+одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да */
+
+bool Squar(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (Squar(number)) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
