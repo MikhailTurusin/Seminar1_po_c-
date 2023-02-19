5d810b2381f8098aa6bf6e834b3afa85e45102b6
MikhailT <m.tyrysin@gmail.com>
2023-02-19 21:51:40 +0300

    Updated Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,34 +1,12 @@
-﻿Console.Clear();
-Console.WriteLine("Введите число: ");
-string number = (Console.ReadLine());
-if (number == "1")
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine($"Заданный номер является Понедельником");
-else if (number == "2")
+int Weekend (int num)
 {
-    Console.WriteLine($"Заданный номер является Вторником");
+            if (num > 5 && num < 8) Console.WriteLine ("Выходной день");
+            else Console.WriteLine ("Будний день");
+            return num;
 }
-else if (number == "3")
-{
-    Console.WriteLine($"Заданный номер является Средой");
-}
-else if (number == "4")
-{
-    Console.WriteLine($"Заданный номер является Четвергом");
-}
-else if (number == "5")
-{
-    Console.WriteLine($"Заданный номер является Пятницей");
-}
-else if (number == "6")
-{
-    Console.WriteLine($"Заданный номер является Субботой");
-}
-else if (number == "7")
-{
-    Console.WriteLine($"Заданный номер является Воскресеньем");
-}
-else
-{
-    Console.WriteLine($"Заданный номер не является днем недели");
-}
+
+if (number > 0 && number < 8) Weekend (number);
+else Console.WriteLine ("Введеное число не является днем недели");
