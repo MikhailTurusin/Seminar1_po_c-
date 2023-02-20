e794f9ecb8c5075ec5e2759c57eb1c9c4713dead
MikhailT <m.tyrysin@gmail.com>
2023-02-20 20:45:50 +0300

    Added Task13

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,6 +5,12 @@
 // 12821 -> да
 // 23432 -> да
 
+bool Negative(int num)
+    {
+    if (num > 0) return num > 9999 && num < 100000;
+    else return num < -9999 && num > -100000;
+    }
+
 bool Palindrome (int num)
 {
     int firstDigit = num / 10000;
@@ -13,13 +19,19 @@ bool Palindrome (int num)
     int secondDigit1 = secondDigit % 10;
     int fourthDigit = num % 100;
     int fourthDigit1 = fourthDigit / 10;
-        return firstDigit==fifthDigit && secondDigit1==fourthDigit1;
+    return firstDigit==fifthDigit && secondDigit1==fourthDigit1;
 }
 
 Console.Write("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number > 99999 | number < 10000) Console.Write ("Введено не пятизначное число");
-else 
-if (Palindrome(number)) Console.Write("Введенное число является палиндромом");
-    else {Console.Write ("Введенное число не палиндром");}
+if (number == 0) Console.Write ("Введенно не 5ти значное число");
+else
+{
+    if (Negative(number))
+        if (Palindrome(number)) Console.Write("Введенное число является палиндромом");
+        else {Console.Write ("Введен не палиндром");}
+    else
+        if (Palindrome(number)) Console.Write("Введенное число является палиндромом");
+        else {Console.Write ("Введенно не 5ти значное число");}
+}
