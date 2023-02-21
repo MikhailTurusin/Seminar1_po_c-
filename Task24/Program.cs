7d51bdb6ac9a343df6daae4dd85fc1e4967b48d0
MikhailT <m.tyrysin@gmail.com>
2023-02-22 00:09:22 +0300

    Added Task24 and Task26

---
+++ Task24/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 24: Напишите программу, которая
+// 1. принимает на вход число (А) и
+// 2. выдаёт сумму чисел от 1 до А.
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int num)
+{
+    int sum = 0;
+    for (int index = 1; index <= num; index++)
+    {
+        sum = sum + index;
+    }
+    return sum;
+}
+
+int result= Sum(number);
+Console.WriteLine ($"Сумма чисел от 1 до {number} = {result}");
---
+++ Task26/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 26: Напишите программу, которая
+// 1. принимает на вход число и
+// 2. выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberofDigits(int num)
+{
+    int count = 1;
+    if (num == 0) return count;
+    else
+    {
+        while (num > 9)
+           {
+            num = num / 10;
+            count = count + 1;
+           }
+    }
+    return count;
+}
+
+int result1 = NumberofDigits(number);
+Console.WriteLine(result1);
