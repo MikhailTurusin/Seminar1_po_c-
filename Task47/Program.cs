9c4b2c30967a91600b229014973bec665789726c
MikhailT <m.tyrysin@gmail.com>
2023-03-05 19:20:07 +0300

    Added Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 47:
+// 1. Задайте двумерный массив размером mxn,
+// 2. заполненный случайными вещественными числами.
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.NextDouble() * (max - min) + min;
+            matrix[i, j] = Math.Round(matrix[i, j], 1, MidpointRounding.ToZero);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array2d = CreateMatrixRndDouble(4, 4, 5, 25);
+PrintMatrix(array2d);
