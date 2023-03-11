5ec93ab49143e2c855a0e3c8566fe004dba01f11
MikhailT <m.tyrysin@gmail.com>
2023-03-11 18:24:03 +0300

    Added Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 54:
+// 1. Задайте двумерный массив.
+// 2. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void OrderingDescendingElem(int[,] matrix)
+{
+    int count = 1;
+    while (count < matrix.GetLength(1))
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = temp;
+                }
+
+            }
+        }
+        count++;
+    }
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 1, 9);
+PrintMatrix(array2d);
+Console.WriteLine();
+OrderingDescendingElem(array2d);
+PrintMatrix(array2d);
