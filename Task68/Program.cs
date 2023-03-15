045462315b48e1cb376b40f43bda8b8bdfc4c338
MikhailT <m.tyrysin@gmail.com>
2023-03-15 09:30:57 +0300

    Updated Task68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -18,6 +18,6 @@ Console.WriteLine(FunctionAkkermana(number1,number2));
 int FunctionAkkermana(int num1, int num2)
 {
     if (num1 == 0) return num2 + 1;
-    else if (num2 == 0) return FunctionAkkermana(num1-1, 1);
-    else return FunctionAkkermana(num1-1, FunctionAkkermana(num1, num2-1));
+    if (num2 == 0) return FunctionAkkermana(num1-1, 1);
+    return FunctionAkkermana(num1-1, FunctionAkkermana(num1, num2-1));
 }
